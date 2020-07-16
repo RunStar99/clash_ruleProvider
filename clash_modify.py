795857c1db3f821c220fadad9ff7f1ff29cb68e9
Runstar <runstar@RunstardeMacBook-Pro.local>
2020-07-16 22:09:32 +0800

    update EasyPrivacy

---
+++ clash_modify.py
@@ -0,0 +1,17 @@
+def main():
+    clash_lines = ''
+    with open('EasyPrivacy.yaml', 'r+') as clash_rule_origine:
+        for clash_line in clash_rule_origine:
+            if ',' in clash_line:
+                clash_lines += '  - ' + clash_line
+        clash_lines = "payload:\n" + clash_lines
+        # print(clash_lines)
+    with open('EasyPrivacy.yaml', 'w') as clash_rule:
+        clash_rule.write(clash_lines)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ update_EasyPrivacy.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanEasyPrivacy.list -O EasyPrivacy.yaml
+python3 clash_modify.py
