0efc310a25cce4dd5c6e9a6065de1ba05fc1932b
Runstar <runstar@RunstardeMacBook-Pro.local>
2020-07-22 15:26:42 +0800

    update EasyPrivacy

--- update_EasyPrivacy.sh
+++ update_EasyPrivacy.sh
@@ -1,5 +1,5 @@
 wget https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanEasyPrivacy.list -O EasyPrivacy.yaml
 python3 clash_modify.py
-git add EasyPrivacy.yaml
+git add .
 git commit -m 'update EasyPrivacy'
 git push origin master
